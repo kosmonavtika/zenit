b19269e152bc14ffd0231c7923ea23dc000eb2c6
kosmonavtika <ops@io.ax>
2020-06-10 16:24:16 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+
+SECONDS=0
+
 docker build . -t mark7-firmware && \
 docker run --rm -v "$PWD/artifacts:/tmp/artifacts" mark7-firmware:latest
+
+echo
+echo "kosmos reader firmware built in $SECONDSs!"
+echo "use \`./flash.sh <target mountpoint> <ssh key>\` to flash device"
