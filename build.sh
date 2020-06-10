3a0b0997bfcd6d2559162c9e49e8de2fac05459c
kosmonavtika <ops@io.ax>
2020-06-10 16:02:25 +0200

    merge build.sh and run.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker build . -t mark7-firmware
+docker build . -t mark7-firmware && \
+docker run --rm -v "$PWD/artifacts:/tmp/artifacts" mark7-firmware:latest
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run --rm -v "$PWD/artifacts:/tmp/artifacts" mark7-firmware:latest
