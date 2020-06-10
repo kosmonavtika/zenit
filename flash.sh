7a9ee5c05790d91e88a6ffdfab49917ef0e0a5db
kosmonavtika <ops@io.ax>
2020-06-10 19:32:40 +0200

    fix flash.sh

--- flash.sh
+++ flash.sh
@@ -2,9 +2,10 @@
 READER_TARGET=$1
 READER_SSH_KEY=$2
 
-rm -rf $1/.kosmos
-rm -f $1/.kobo/KoboRoot.tgz
+rm -rfv $1/.kosmos
+rm -fv $1/.kobo/KoboRoot.tgz
 
+mkdir -pv $READER_TARGET/.kosmos/
 cp -vrf template/* $READER_TARGET/.kosmos/
 cp -vrf artifacts/root/* $READER_TARGET/.kosmos/
 cp -vrf artifacts/vendor/* $READER_TARGET/.kobo/
