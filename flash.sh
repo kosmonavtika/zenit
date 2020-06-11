220db3368328c63349a15f3c36b8edb1877aa916
kosmonavtika <ops@io.ax>
2020-06-11 02:31:52 +0200

    add timer to flash.sh

--- flash.sh
+++ flash.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+SECONDS=0
 READER_TARGET=$1
 READER_SSH_KEY=$2
 
@@ -9,3 +11,7 @@ mkdir -pv $READER_TARGET/.kosmos/
 cp -vrf template/* $READER_TARGET/.kosmos/
 cp -vrf artifacts/root/* $READER_TARGET/.kosmos/
 cp -vrf artifacts/vendor/* $READER_TARGET/.kobo/
+
+duration=$SECONDS
+echo
+echo "kosmos reader flashed in ${duration}s"
